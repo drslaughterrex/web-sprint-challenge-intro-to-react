f6cd23b2a8cdd653100ad047c48a3ef141814c0f
drslaughterrex <drslaughterrex@gmail.com>
2021-04-16 13:32:46 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Characters from "./components/Character";
+import "./App.css";
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+	// Try to think through what state you'll need for this app before starting. Then build out
+	// the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+	// Fetch characters from the API in an effect hook. Remember, anytime you have a
+	// side effect in a component, you want to think about which state and/or props it should
+	// sync up with, if any.
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
-}
+	const [char, setChar] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get("https://swapi.dev/api/people/")
+			.then((res) => {
+				setChar(res.data);
+			})
+			.catch((err) => console.log('Lord Vader, there was an error.', err));
+	}, []);
+
+	return (
+		<div className="App">
+			<h1 className="Header">Characters</h1>
+			<div className="container">
+				<Characters charName={char} />
+			</div>
+		</div>
+	);
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,52 @@
+import React from "react";
+import styled from "styled-components";
+
+const Cards = styled.div`
+	width: 80%;
+	margin: 1.5rem auto;
+	background-image:url('https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fstartswithabang%2Ffiles%2F2017%2F10%2FTiny_bit_of_U.jpg');
+	display: flex;
+	border: 4px solid blue;
+	justify-content: center;
+	ul {
+		list-style: none;
+	}
+	h2 {
+		color: green;
+		font-size: 2rem;
+		font-weight: bolder;
+	}
+	p {
+		color: white;
+		font-size: 1.5rem;
+		font-weight: bold;
+	}
+`;
+
 // Write your Character component here
+export default function Character(props) {
+	return (
+		<>
+			{props.charName.map((e) => {
+				return <CardMaker key={e.name} char={e} />;
+			})}
+		</>
+	);
+}
+
+const CardMaker = ({ char }) => {
+	return (
+		<Cards>
+			<ul>
+				<li>
+					<h2>Character Name: {char.name}</h2>
+					<p>Birth Year: {char.birth_year}</p>
+					<p>Gender: {char.gender}</p>
+					<p>Hair Color: {char.hair_color}</p>
+                    <p>Eye Color: {char.eye_color}</p>
+                    <p>Height: {char.height}</p>
+				</li>
+			</ul>
+		</Cards>
+	);
+};
